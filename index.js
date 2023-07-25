282239467b2e52395df4b624a3a4c2e99851c53e
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-25 16:43:14 +0200

    allow origin & headers

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const products_routes = require('./routes/products')
+const bodyParser = require('body-parser')
 
 require('dotenv').config()
 
 mongoose.connect(process.env.MONGO_URI).then((result)=>app.listen(8080, ()=>console.log('server start on port 8080'))).catch((err)=>console.log(err))
 
-app.use(express.json())
+app.use(bodyParser.json())
+app.use((req,res,next)=>{res.header('Access-Control-Allow-Origin', 'http://localhost:5173');next()})
+app.use((req,res,next)=>{res.header('Access-Control-Allow-Headers', 'Content-Type');next()})
 app.use('/api/products', products_routes)
