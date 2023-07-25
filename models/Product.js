aec3b95713d184ccedee6f85b11a879354174d40
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-25 16:42:57 +0200

    constraints

--- models/Product.js
+++ models/Product.js
@@ -1,9 +1,19 @@
 const mongoose = require('mongoose')
 
 const ProductSchema = new mongoose.Schema({
-  name:String,
+  name: {
+    type: String,
+    required: [true, 'Le produit doit possèder un nom'],
+    min: [3, 'Le nom du produit doit possèder au moins 3 caractères'],
+    max: [20, 'Le nom du produit doit possèder moins de 20 caractères']
+  },
   slug:String,
-  price:Number
+  price: {
+    type: Number,
+    required: [true, 'Le produit doit possèder un prix'],
+    min: [1, 'Le prix du produit doit être au moins de 1'],
+    max: [5000, 'Le prix du produit ne doit pas dépasser 5000']
+  }
 })
 
 const Product = mongoose.model('Product', ProductSchema)
