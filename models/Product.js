37011934d5e5a09f57475adfb2694c9177a7ad22
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-27 17:17:34 +0200

    custom unique name validator

--- models/Product.js
+++ models/Product.js
@@ -1,21 +1,32 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Le produit doit possèder un nom'],
-    min: [3, 'Le nom du produit doit possèder au moins 3 caractères'],
-    max: [20, 'Le nom du produit doit possèder moins de 20 caractères']
+    required: [true, "Le produit doit possèder un nom"],
+    min: [3, "Le nom du produit doit possèder au moins 3 caractères"],
+    max: [20, "Le nom du produit doit possèder moins de 20 caractères"],
+    unique: true,
+    validate: {
+      validator: async (name) => {
+        return (await Product.findOne({
+          name: { $regex: `^${name}$`, $options: "i" },
+        }))
+          ? false
+          : true;
+      },
+      message: "Nom déjà utilisé",
+    },
   },
-  slug:String,
+  slug: String,
   price: {
     type: Number,
-    required: [true, 'Le produit doit possèder un prix'],
-    min: [1, 'Le prix du produit doit être au moins de 1'],
-    max: [5000, 'Le prix du produit ne doit pas dépasser 5000']
-  }
-})
+    required: [true, "Le produit doit possèder un prix"],
+    min: [2, "Le prix du produit doit être au moins de 1"],
+    max: [5000, "Le prix du produit ne doit pas dépasser 5000"],
+  },
+});
 
-const Product = mongoose.model('Product', ProductSchema)
+const Product = mongoose.model("Product", ProductSchema);
 
-module.exports = Product
+module.exports = Product;
