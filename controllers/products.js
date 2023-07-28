4a5a38ff612ea261e74beeb28e3df273534ab7cc
Pierre Ledoux <Pierre.LEDOUX@metarom.fr>
2023-07-28 17:16:16 +0200

    return message if product not found

--- controllers/products.js
+++ controllers/products.js
@@ -1,58 +1,68 @@
-const Product = require('../models/Product.js')
+const Product = require("../models/Product.js");
 
-const getProducts =( (req,res,next)=> {
+const getProducts = (req, res, next) => {
   Product.find({})
-  .then(result=>res.status(200).json(result))
-  .catch(error=>res.status(500).json(error))
-});
+    .then((result) => res.status(200).json(result))
+    .catch((error) => res.status(500).json(error));
+};
 
-const getProduct = ((req,res,next) =>{
-  Product.findOne({'name': {'$regex': `^${req.params.name}$`, $options: 'i'}})
-  .then(result=>result ? res.status(200).json(result) : res.status(404).json(null) )
-  .catch(error=>res.status(500).json(error))
-})
+const getProduct = (req, res, next) => {
+  Product.findOne({ name: { $regex: `^${req.params.name}$`, $options: "i" } })
+    .then((result) =>
+      result
+        ? res.status(200).json(result)
+        : res.status(404).json({ message: "Product not found" })
+    )
+    .catch((error) => res.status(500).json(error));
+};
 
-const createProduct = (req,res,next)=>{
-  req.body.slug = req.body.name
-  if(!req.body){
-    res.status(400).json({message:'Body is missing or invalid'})
+const createProduct = (req, res, next) => {
+  req.body.slug = req.body.name;
+  if (!req.body) {
+    res.status(400).json({ message: "Body is missing or invalid" });
   }
   Product.create(req.body)
-  .then(result=>res.status(201).json(result))
-  .catch(error=>res.status(500).json(error))
-}
+    .then((result) => res.status(201).json(result))
+    .catch((error) => res.status(400).json(error));
+};
 
-const updateProduct = (req,res)=>{
-  req.params.id ? null : res.status(400).json({message:'Id is missing'})
-  req.body ? null : res.status(400).json({message:'Body is missing or invalid'})
-  
-  Product.findOneAndUpdate({_id:req.params.id}, req.body, {new:true})
-  .then(result=>res.status(200).json(result))
-  .catch(error=>res.status(404).json(error))
-}
+const updateProduct = (req, res) => {
+  req.params.id ? null : res.status(400).json({ message: "Id is missing" });
+  req.body
+    ? null
+    : res.status(400).json({ message: "Body is missing or invalid" });
 
-const replaceProduct = (req,res)=>{
-  req.params.id?null:res.status(400).json({message:'ID is missing'})
-  req.body?null:res.status(400).json({message:'Body is missing'})
+  Product.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+    .then((result) => res.status(200).json(result))
+    .catch((error) => res.status(404).json(error));
+};
 
-  Product.findOneAndReplace({_id:req.params.id}, req.body, {new:true})
-  .then(result => res.status(200).json(result))
-  .catch(error=>res.status(500).json(error))
-}
+const replaceProduct = (req, res) => {
+  req.params.id ? null : res.status(400).json({ message: "ID is missing" });
+  req.body ? null : res.status(400).json({ message: "Body is missing" });
 
-const deleteProduct = (req,res,next)=>{
-  req.params.id ? null : res.status(400).json({message: 'ID is missing'})
+  Product.findOneAndReplace({ _id: req.params.id }, req.body, { new: true })
+    .then((result) => res.status(200).json(result))
+    .catch((error) => res.status(500).json(error));
+};
 
-  Product.findOneAndDelete({_id:req.params.id})
-  .then(result=>res.status(200).json({message: `${result.name} has been deleted`}))
-  .catch(error=>res.status(404).json(error))
-}
+const deleteProduct = (req, res, next) => {
+  req.params.id ? null : res.status(400).json({ message: "ID is missing" });
 
-module.exports ={
+  Product.findOneAndDelete({ _id: req.params.id })
+    .then((result) =>
+      res
+        .status(200)
+        .json({ message: `${result.name} has been deleted`, success: true })
+    )
+    .catch((error) => res.status(404).json(error));
+};
+
+module.exports = {
   getProduct,
   getProducts,
   createProduct,
   updateProduct,
   replaceProduct,
-  deleteProduct
-}
+  deleteProduct,
+};
